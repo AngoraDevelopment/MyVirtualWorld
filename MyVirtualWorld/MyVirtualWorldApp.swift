09b667b728066484fdd090cd74a06cd01611e7e0
Edgardo <smokeshadowedgardo123@gmail.com>
2026-04-08 15:39:56 -0700

    Initial Commit

---
+++ MyVirtualWorld/MyVirtualWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVirtualWorldApp.swift
+//  MyVirtualWorld
+//
+//  Created by Edgardo Ramos on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MyVirtualWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
